0fdb760a52407f4c364623fc5d4f7264e3f2b46c
rafaelspa <rsdepaula67@gmail.com>
2023-01-05 11:23:51 -0300

    GET categories

--- src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.devsuperior.dscommerce.repositories;
 
-import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
