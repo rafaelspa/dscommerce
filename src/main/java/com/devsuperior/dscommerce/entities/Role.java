f5a1f30b98277129e2217e7ae64b37fbfb4513be
rafaelspa <rsdepaula67@gmail.com>
2023-01-04 21:24:53 -0300

    OAuth2 JWT checklist

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,12 +1,14 @@
 package com.devsuperior.dscommerce.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,6 +34,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return Authority;
     }
