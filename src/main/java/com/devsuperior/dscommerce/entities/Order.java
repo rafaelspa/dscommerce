14e41b26a3a37140ca56d89af935651e04cd48b1
rafaelspa <rsdepaula67@gmail.com>
2023-01-21 20:50:14 -0300

    Heroku ready

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -4,85 +4,104 @@ import javax.persistence.*;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
 public class Order {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant moment;
-    private OrderStatus status;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
+	private OrderStatus status;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
 
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
-    @OneToMany(mappedBy = "id.order")
-    private Set<OrderItem> items = new HashSet<>();
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
-    public Order() {
-    }
+	public Order() {
+	}
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
-        this.id = id;
-        this.moment = moment;
-        this.status = status;
-        this.client = client;
-    }
+	public Order(Long id, Instant moment, OrderStatus status, User client) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Instant getMoment() {
-        return moment;
-    }
+	public Instant getMoment() {
+		return moment;
+	}
 
-    public void setMoment(Instant moment) {
-        this.moment = moment;
-    }
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
 
-    public OrderStatus getStatus() {
-        return status;
-    }
+	public OrderStatus getStatus() {
+		return status;
+	}
 
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
 
-    public User getClient() {
-        return client;
-    }
+	public User getClient() {
+		return client;
+	}
 
-    public void setClient(User client) {
-        this.client = client;
-    }
+	public void setClient(User client) {
+		this.client = client;
+	}
 
-    public Payment getPayment() {
-        return payment;
-    }
+	public Payment getPayment() {
+		return payment;
+	}
 
-    public void setPayment(Payment payment) {
-        this.payment = payment;
-    }
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
-    public Set<OrderItem> getItems() {
-        return items;
-    }
+	public Set<OrderItem> getItems() {
+		return items;
+	}
 
-    public List<Product> getProducts() {
-        return items.stream().map(OrderItem::getProduct).toList();
-    }
+	public List<Product> getProducts() {
+		return items.stream().map(OrderItem::getProduct).toList();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+
 }
--- src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
@@ -4,34 +4,58 @@ import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class OrderItemPK implements Serializable {
 
-    @ManyToOne
-    @JoinColumn(name = "order_id")
-    private Order order;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    private Product product;
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
 
-    public OrderItemPK() {
-    }
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
 
-    public Order getOrder() {
-        return order;
-    }
+	public OrderItemPK() {
+	}
 
-    public void setOrder(Order order) {
-        this.order = order;
-    }
+	public Order getOrder() {
+		return order;
+	}
 
-    public Product getProduct() {
-        return product;
-    }
+	public void setOrder(Order order) {
+		this.order = order;
+	}
 
-    public void setProduct(Product product) {
-        this.product = product;
-    }
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+
 }
